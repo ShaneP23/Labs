a1b4dc5619a2cfda7116c736d7b512508f637d4e
spete <spete@LAPTOP-875F4LBO>
2024-03-12 21:05:36 -0400

    Add project files.

---
+++ Lab 3/Form1.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace Lab_3
+{
+    partial class lab3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // lab3
+            // 
+            this.ClientSize = new System.Drawing.Size(378, 344);
+            this.Name = "lab3";
+            this.Text = "Lab 3";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label radiusOfSphereLbl;
+        private System.Windows.Forms.TextBox radiusOfSphereTxtBox;
+        private System.Windows.Forms.Label diameterLbl;
+        private System.Windows.Forms.Label surfaceAreaLbl;
+        private System.Windows.Forms.Label volumeLbl;
+        private System.Windows.Forms.Label diameterOutputLbl;
+        private System.Windows.Forms.Label surfaceAreaOutputLbl;
+        private System.Windows.Forms.Label volumeOutputLbl;
+        private System.Windows.Forms.Button calculateBtn;
+        private System.Windows.Forms.PictureBox ballAndSphere1;
+        private System.Windows.Forms.PictureBox ballAndSphere2;
+    }
+}
+
---
+++ Lab 3/Form1.cs
@@ -0,0 +1,45 @@
+﻿//Lab 3
+//Due:2-13-22
+//CIS 199-03
+//Grading ID:S2223
+//The purpose of this lab is to calculate the diameter, surface area, and volume.
+//The user inputs the radius and the outputs are calculated and presented.
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_3
+{
+    public partial class lab3 : Form
+    {
+        public lab3()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateBtn_Click(object sender, EventArgs e)   //click calculate button
+        {
+            double Radius; //radius entered by user
+            double Diameter; //Diameter
+            double SurfaceA; //Surface area
+            double Volume; //voulme
+
+            Radius = double.Parse(radiusOfSphereTxtBox.Text);  //system reads the radius entered
+
+            Diameter = 2 * Radius; //calculates diameter
+            SurfaceA= 4*Math.PI * Math.Pow(Radius,2);  //calculates surface area
+            Volume = (4 * Math.PI * Math.Pow(Radius, 3)) / 3;  //calculates volume
+
+            diameterOutputLbl.Text = "{Diameter}";  //Assign the diameter calculation to the diameter output label
+            surfaceAreaOutputLbl.Text = "{SurfaceA}";  //Assign the surface area calculation to the surface area label
+            volumeOutputLbl.Text = "{Volume}";  //Assign the volume calculation to the volume label
+        }
+
+    }
+}
